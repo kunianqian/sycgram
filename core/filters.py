860da0b3cb07daede38e9ee3ba7ff7279e328fd9
kunianqian <nsxzde@proton.me>
2025-05-07 01:36:25 -0600

    Update filters.py

--- core/filters.py
+++ core/filters.py
@@ -5,9 +5,11 @@ from pyrogram.types import Message
 from tools.constants import STORE_TRACE_DATA, COMMAND_YML
 from tools.storage import SimpleStore
 
+# 从 YAML 配置文件中加载命令数据
 CMDS_DATA: Dict[str, Any] = yaml.full_load(open(COMMAND_YML, 'rb'))
-CMDS_PREFIX = CMDS_DATA['help'].get("all_prefixes")
 
+# 从配置中获取所有命令的前缀
+CMDS_PREFIX = CMDS_DATA['help'].get("all_prefixes")
 
 """
 匹配UserBot指令
@@ -15,8 +17,11 @@ CMDS_PREFIX = CMDS_DATA['help'].get("all_prefixes")
 def command(key: str):
     # 直接访问cmd值，因为已知它一定存在
     cmd_alias = CMDS_DATA[key].get("cmd")
+    
     # 仅当别名与key不同，才添加到cmd列表中
     cmd = [key] if cmd_alias == key else [key, cmd_alias]
+    
+    # 返回过滤器，用于匹配命令
     return filters.me & filters.text & filters.command(cmd, CMDS_PREFIX)
 
 """
